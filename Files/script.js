d5c1d87dd0643c40ae6c28978756695a46b26e11
Yutao Xue <164786071+ytx-26@users.noreply.github.com>
2024-04-27 14:40:26 +0100

    Creating a function for default box generation

--- Files/script.js
+++ Files/script.js
@@ -1,14 +1,16 @@
 //references the 'container' id in index.html
 let container = document.getElementById("container");
 
-for (let i = 0; i < 16*16; i++){
-    //creates a new div element
-    let newDiv = document.createElement("div"); 
-
-    //adds the new div to be inside of 'container'
-    container.appendChild(newDiv);
-    //adds class 'boxes' to the divs
-    newDiv.classList.add("boxes");
+function defaultBoxDimensions(){
+    for (let i = 0; i < 16*16; i++){
+        //creates a new div element
+        let newDiv = document.createElement("div"); 
+    
+        //adds the new div to be inside of 'container'
+        container.appendChild(newDiv);
+        //adds class 'boxes' to the divs
+        newDiv.classList.add("boxes");
+    }
 }
 
 //Grid input 
